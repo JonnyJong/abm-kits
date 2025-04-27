5c6d900b558d17f2cc6d18b464b858ede7b9b758
Jonny <jonnyjong@outlook.com>
2025-04-27 09:32:05 +0800

    🐳 chore(ui): Optimized style monitoring features

--- scripts/build.js
+++ scripts/build.js
@@ -8,6 +8,7 @@ const DIST = path.join(PACKAGE_ROOT, 'dist');
 const STYLE_ROOT = path.join(PACKAGE_ROOT, 'styles/widgets');
 
 const PATTERN_IMPORT = /import ([A-Z0-9_]+) from '([a-z0-9-]+)\.style';/g;
+const PATTERN_CONST = /const (.+) = .+; \/\/ style:(.+)/g;
 const PATTERN_CSS_SELECTOR = /(?<=^|\}|,):host([^{,>]+?)(?=\{| |,|>)/g;
 
 /**
@@ -26,28 +27,57 @@ function getCSS(mod) {
 		return `:host(${selector})`;
 	});
 
-	return css;
+	return `*{box-sizing:border-box;}${css}`;
 }
 
 /**
  * @param {string} file
+ * @returns {string[]}
  */
 function compile(file) {
 	let js = fs.readFileSync(file, 'utf8');
-	let changed = false;
-	js = js.replace(PATTERN_IMPORT, (origin, label, mod) => {
-		if (!(label && mod)) return origin;
-		changed = true;
-		return `const ${label} = ${JSON.stringify(getCSS(mod))};`;
-	});
-	if (!changed) return;
+	const mods = [];
+	js = js
+		.replace(PATTERN_IMPORT, (origin, label, mod) => {
+			if (!(label && mod)) return origin;
+			mods.push(mod);
+			return `const ${label} = ${JSON.stringify(getCSS(mod))}; // style:${mod}`;
+		})
+		.replace(PATTERN_CONST, (origin, label, mod) => {
+			if (!(label && mod)) return origin;
+			mods.push(mod);
+			return `const ${label} = ${JSON.stringify(getCSS(mod))}; // style:${mod}`;
+		});
+	if (mods.length === 0) return mods;
 	fs.writeFileSync(file, js, 'utf8');
+	return mods;
+}
+
+/**
+ * @param {string} file
+ * @returns {string[]}
+ */
+function getMods(file) {
+	const js = fs.readFileSync(file, 'utf8');
+	const mods = [];
+	js
+		.replace(PATTERN_IMPORT, (origin, label, mod) => {
+			if (!(label && mod)) return origin;
+			mods.push(mod);
+		})
+		.replace(PATTERN_CONST, (origin, label, mod) => {
+			if (!(label && mod)) return origin;
+			mods.push(mod);
+		});
+	return mods;
 }
 
 module.exports = {
 	PACKAGE_ROOT,
 	DIST,
+	STYLE_ROOT,
 	compile,
+	getMods,
 };
 
 if (require.main === module) {
--- scripts/watch.js
+++ scripts/watch.js
@@ -1,5 +1,7 @@
 const chokidar = require('chokidar');
-const { compile, DIST } = require('./build');
+const { compile, DIST, STYLE_ROOT, getMods } = require('./build');
+const path = require('node:path');
+const { readdirSync } = require('node:fs');
 
 /// <reference types="node:fs" />
 
@@ -7,24 +9,54 @@ const { compile, DIST } = require('./build');
  * @type {Map<string, number>}
  */
 const mTimes = new Map();
+/**
+ * @type {Map<string, string[]>}
+ */
+const mods = new Map();
 
 /**
  * @param {string} file
  * @param {Stats} stat
  */
-function handler(file, stat) {
+function tsHandler(file, stat) {
 	if (!file.endsWith('.js')) return;
 	if (!stat.isFile()) return;
 	if (stat.mtimeMs <= (mTimes.get(file) ?? 0)) return;
 	console.log(`compiling ${file}`);
-	compile(file);
+	mods.set(file, compile(file));
 	mTimes.set(file, Date.now());
 }
 
-const watcher = chokidar.watch(DIST, {
+function styleHandler(file, stat) {
+	if (!file.endsWith('.styl')) return;
+	if (!stat.isFile()) return;
+	const { name } = path.parse(file);
+	for (const [file, styles] of mods) {
+		if (!styles.includes(name)) continue;
+		console.log(`compiling ${file}`);
+		mods.set(file, compile(file));
+		mTimes.set(file, Date.now());
+	}
+}
+
+for (const dirent of readdirSync(DIST, {
+	withFileTypes: true,
+	recursive: true,
+})) {
+	if (!dirent.isFile()) continue;
+	if (path.extname(dirent.name) !== '.js') continue;
+	const file = path.join(dirent.parentPath, dirent.name);
+	mods.set(file, getMods(file));
+}
+
+const watcher = chokidar.watch([DIST, STYLE_ROOT], {
 	awaitWriteFinish: true,
 });
 
 watcher.on('ready', () => {
-	watcher.on('change', handler).on('add', handler);
+	watcher
+		.on('change', tsHandler)
+		.on('add', tsHandler)
+		.on('change', styleHandler)
+		.on('add', styleHandler);
 });
