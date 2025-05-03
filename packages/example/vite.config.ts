6d93d2d5df31fb008da479b8207a4b4301486aef
Jonny <jonnyjong@outlook.com>
2025-05-03 11:00:26 +0800

    🐳 chore: Fix demo assets path

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -5,23 +5,27 @@ const HOST = '127.0.0.1';
 const PORT = 5500;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	clearScreen: false,
-	server: {
-		port: PORT,
-		strictPort: true,
-		host: HOST,
-		hmr: {
-			protocol: 'ws',
+export default defineConfig(({ mode }) => {
+	return {
+		clearScreen: false,
+		server: {
+			port: PORT,
+			strictPort: true,
 			host: HOST,
-			port: PORT + 1,
+			hmr: {
+				protocol: 'ws',
+				host: HOST,
+				port: PORT + 1,
+			},
 		},
-	},
-	optimizeDeps: {
-		force: true,
-	},
-	plugins: [pugVite()],
-	build: {
-		outDir: '../../demo',
-	},
+		optimizeDeps: {
+			force: true,
+		},
+		plugins: [pugVite()],
+		build: {
+			outDir: '../../demo',
+			emptyOutDir: true,
+		},
+		base: mode === 'production' ? '/abm-kits/' : '/',
+	};
 });
